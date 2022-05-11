daaefa4bec7156ed91b9a1a0526b91096b12c31e
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-05-11 22:31:22 +0900

    Add PhotoOrganizerApp and PhotoOrganizer projects

---
+++ PhotoOrganizerApp/App.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml;
+
+namespace PhotoOrganizerApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+
+    private Window? _window;
+}
---
+++ PhotoOrganizerApp/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace PhotoOrganizerApp;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
