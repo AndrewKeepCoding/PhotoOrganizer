9d2d5b6700fcb5c8648056d3b7029a5560477f03
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-05-14 15:18:58 +0900

    Show file count on TreeViewItems

--- PhotoOrganizerApp/ViewModels/OutputFolderNodeViewModel.cs
+++ PhotoOrganizerApp/ViewModels/OutputFolderNodeViewModel.cs
@@ -1,28 +1,63 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 
 namespace PhotoOrganizings.ViewModels;
 
-public class OutputFolderNodeViewModel : OutputNodeViewModelBase
+public partial class OutputFolderNodeViewModel : OutputNodeViewModelBase
 {
+    [ObservableProperty]
+    private int _fileNodesCount = 0;
+
     public OutputFolderNodeViewModel(string name) : base(name)
     {
     }
 
     public ObservableCollection<OutputNodeViewModelBase> Children { get; private set; } = new();
-    //public ObservableCollection<OutputFolderNodeViewModel> FolderChildren { get; private set; } = new();
-    //public ObservableCollection<OutputFileNodeViewModel> FileChildren { get; private set; } = new();
 
-    public void AddFolderNode(OutputFolderNodeViewModel folderNode)
+    protected void AddFileNode(OutputFileNodeViewModel fileNode, string[] folderHierarchy)
     {
-        //FolderChildren ??= new();
-        //FolderChildren.Add(folderNode);
-        Children.Add(folderNode);
+        string folderName = folderHierarchy.FirstOrDefault() ?? string.Empty;
+        if (folderName.Length > 0)
+        {
+            OutputFolderNodeViewModel? folderNode = Children
+                .OfType<OutputFolderNodeViewModel>()
+                .Where(f => f.Name == folderName)
+                .FirstOrDefault();
+
+            if (folderNode is null)
+            {
+                folderNode = new OutputFolderNodeViewModel(Path.Combine(this.NodePath, folderName));
+                Children.Add(folderNode);
+            }
+
+            folderNode.AddFileNode(fileNode, folderHierarchy[1..]);
+        }
+        else
+        {
+            Children.Add(fileNode);
+        }
+
+        FileNodesCount = GetFileNodesCount();
     }
 
-    public void AddFileNode(OutputFileNodeViewModel fileNode)
+    protected int GetFileNodesCount()
     {
-        //FileChildren ??= new();
-        //FileChildren.Add(fileNode);
-        Children.Add(fileNode);
+        int fileNodesCount = 0;
+
+        foreach (OutputNodeViewModelBase childNode in Children)
+        {
+            if (childNode is OutputFolderNodeViewModel folder)
+            {
+                fileNodesCount += folder.GetFileNodesCount();
+            }
+            else if (childNode is OutputFileNodeViewModel)
+            {
+                fileNodesCount++;
+            }
+        }
+
+        return fileNodesCount;
     }
 }
--- PhotoOrganizerApp/ViewModels/OutputRootFolderNodeViewModel.cs
+++ PhotoOrganizerApp/ViewModels/OutputRootFolderNodeViewModel.cs
@@ -1,6 +1,3 @@
-using System.IO;
-using System.Linq;
-
 namespace PhotoOrganizings.ViewModels;
 
 public class OutputRootFolderNodeViewModel : OutputFolderNodeViewModel
@@ -11,30 +8,7 @@ public class OutputRootFolderNodeViewModel : OutputFolderNodeViewModel
 
     public void AddFileChild(OutputFileNodeViewModel fileNode)
     {
-        if (fileNode.ParentsHierarchy is not null)
-        {
-            int rootDepth = ParentsHierarchy.Length + 1;
-            OutputFolderNodeViewModel currentFolder = this;
-
-            for (int i = rootDepth; i < fileNode.ParentsHierarchy.Length; i++)
-            {
-                string searchingFolderName = fileNode.ParentsHierarchy[i];
-                OutputFolderNodeViewModel? searchingResult = currentFolder.Children?
-                    .OfType<OutputFolderNodeViewModel>()
-                    .Where(node => node.Name == searchingFolderName)
-                    .FirstOrDefault();
-
-                if (searchingResult is null)
-                {
-                    string newFolderNodePath = Path.Combine(fileNode.ParentsHierarchy[0..(i + 1)]);
-                    searchingResult = new OutputFolderNodeViewModel(newFolderNodePath);
-                    currentFolder.AddFolderNode(searchingResult);
-                }
-
-                currentFolder = searchingResult;
-            }
-
-            currentFolder.AddFileNode(fileNode);
-        }
+        int currentDepth = ParentsHierarchy.Length + 1;
+        AddFileNode(fileNode, fileNode.ParentsHierarchy[currentDepth..]);
     }
 }
